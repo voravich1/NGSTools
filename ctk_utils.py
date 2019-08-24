8fdcc54d7e0b45d4ec7d2b220cf04044d7991cd1
vorav <voravich1@hotmail.com>
2019-08-25 00:58:17 +0700

    complete 2 utils method bug fixed

--- ctk_utils.py
+++ ctk_utils.py
@@ -1,17 +1,49 @@
-class utils:
+import numpy as np
+from pathlib import Path
 
-    def createCountDictFromBedgraph(self,bedgraphFileName):
+def createCountDictFromBedgraph(bedgraphFileName):
 
-        count_bedgraph = np.loadtxt(fname=bedgraphFileName, dtype="str")
+    #count_bedgraph = np.loadtxt(fname=bedgraphFileName, dtype="str")
+    #count_bedgraph = open(bedgraphFileName)
 
+    with open(bedgraphFileName,'r') as count_bedgraph:
         count_dict = {}
-        for count_data in count_bedgraph:
-
-            if (count_data[0] != "" and count_data[0] != "track"):
+        for line in count_bedgraph:
+            if (line != "\n" and not line.startswith("track")):
+                count_data = line.split("\t")
                 start = int(count_data[1])
                 stop = int(count_data[2])
 
-                for i in range(start, stop + 1):
-                    count_dict[i] = count_data[3]
+                for i in range(start, stop):
+                    count_dict[i] = int(count_data[3].split("\n")[0])
 
-        return count_dict
+    return count_dict
+
+def convert2rpmBedgraph(bedgraphFileName,totalread):
+    p = Path(bedgraphFileName)
+    inputFolder = Path(p.parent)
+    fileName = p.stem
+    suffix = p.suffix
+
+    saveFileName = fileName + "_rpm" + suffix
+    bedgraphRPM = inputFolder.joinpath(saveFileName)
+    bedgraphRPMFile = open(bedgraphRPM, 'w')
+
+    rpm_factor = totalread/1000000
+
+    with open(bedgraphFileName,'r') as count_bedgraph:
+        for line in count_bedgraph:
+            if (line != "\n" and not line.startswith("track")):
+                count_data = line.split("\t")
+                chr = count_data[0]
+                start = int(count_data[1])
+                stop = int(count_data[2])
+                count = int(count_data[3].split("\n")[0])
+                rpm_count = round(count/rpm_factor)
+                bedgraphline = chr + "\t" + str(start) + "\t" + str(stop) + "\t" + str(rpm_count) + "\n"
+                bedgraphRPMFile.write(bedgraphline)
+            else:
+                bedgraphRPMFile.write(line)
+
+    bedgraphRPMFile.close()
+
--- post_process_stat_ctk.py
+++ post_process_stat_ctk.py
@@ -1,37 +1,61 @@
-from ctk_utils import utils
+import ctk_utils
 import numpy as np
 import math
 from scipy.stats import fisher_exact
+from pathlib import Path
 
+input_folder = "C:/Users/vorav/Desktop/opas_bgi_set2/test_post_ctk"
+p = Path("C:/Users/vorav/Desktop/opas_bgi_set2/test_post_ctk")
 
-useable_read_pool_inf = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/pool_virus_inf_r2_pool.bed"
-useable_read_pool_non = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/pool_virus_non_r2_pool.bed"
-useable_read_sm_inf = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/sm_virus_inf_r2_pool.bed"
-useable_read_sm_non = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/sm_virus_non_r2_pool.bed"
+useable_read_pool_inf = p / "pool_virus_inf_r2_pool.bed"
+useable_read_pool_non = p / "pool_virus_non_r2_pool.bed"
+useable_read_sm_inf = p / "sm_virus_inf_r2_pool.bed"
+useable_read_sm_non = p / "sm_virus_non_r2_pool.bed"
 
-pool_inf_peak_bed = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/pool_virus_inf_r2_pool_peak_sig.bed"
-pool_non_peak_bed = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/pool_virus_non_r2_pool_peak_sig.bed"
-sm_inf_peak_bed = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/sm_virus_inf_r2_pool_peak_sig.bed"
-sm_non_peak_bed = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/sm_virus_non_r2_pool_peak_sig.bed"
+pool_inf_peak_bed = p / "pool_virus_inf_r2_pool_peak_sig.bed"
+pool_non_peak_bed = p / "pool_virus_non_r2_pool_peak_sig.bed"
+sm_inf_peak_bed = p / "sm_virus_inf_r2_pool_peak_sig.bed"
+sm_non_peak_bed = p / "sm_virus_non_r2_pool_peak_sig.bed"
 
-union_bedgraph_ctk = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/union.bedgraph"
-pool_inf_bedgraph = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/pool_virus_inf_r2_pool.bedgraph"
-pool_non_bedgraph = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/pool_virus_non_r2_pool.bedgraph"
-sm_inf_bedgraph = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/sm_virus_inf_r2_pool.bedgraph"
-sm_non_bedgraph = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/sm_virus_non_r2_pool.bedgraph"
+#union_bedgraph_ctk = "/drives/c/Users/vorav/Desktop/opas_bgi_set2/test_post_ctk/union.bedgraph"
+pool_inf_bedgraph = p / "pool_virus_inf_r2_pool.bedgraph"
+pool_non_bedgraph = p / "pool_virus_non_r2_pool.bedgraph"
+sm_inf_bedgraph = p / "sm_virus_inf_r2_pool.bedgraph"
+sm_non_bedgraph = p / "sm_virus_non_r2_pool.bedgraph"
 
-stat_inf_filename = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/stat_inf.txt"
-stat_non_filename = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/stat_non.txt"
+# Save File name
+stat_inf_filename = p / "stat_inf.txt"
+stat_non_filename = p / "stat_non.txt"
 
-sig_inf_peak_bedgraph = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/sig_peak_inf.bedgraph"
-sig_non_peak_bedgraph = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/sig_peak_non.bedgraph"
+sig_inf_peak_bedgraph = p / "sig_peak_inf.bedgraph"
+sig_non_peak_bedgraph = p / "sig_peak_non.bedgraph"
 
+#useable_read_pool_inf = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/pool_virus_inf_r2_pool.bed"
+#useable_read_pool_non = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/pool_virus_non_r2_pool.bed"
+#useable_read_sm_inf = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/sm_virus_inf_r2_pool.bed"
+#useable_read_sm_non = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/sm_virus_non_r2_pool.bed"
+
+#pool_inf_peak_bed = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/pool_virus_inf_r2_pool_peak_sig.bed"
+#pool_non_peak_bed = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/pool_virus_non_r2_pool_peak_sig.bed"
+#sm_inf_peak_bed = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/sm_virus_inf_r2_pool_peak_sig.bed"
+#sm_non_peak_bed = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/sm_virus_non_r2_pool_peak_sig.bed"
+
+#union_bedgraph_ctk = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/union.bedgraph"
+#pool_inf_bedgraph = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/pool_virus_inf_r2_pool.bedgraph"
+#pool_non_bedgraph = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/pool_virus_non_r2_pool.bedgraph"
+#sm_inf_bedgraph = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/sm_virus_inf_r2_pool.bedgraph"
+#sm_non_bedgraph = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/sm_virus_non_r2_pool.bedgraph"
+
+#stat_inf_filename = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/stat_inf.txt"
+#stat_non_filename = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/stat_non.txt"
+
+#sig_inf_peak_bedgraph = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/sig_peak_inf.bedgraph"
+#sig_non_peak_bedgraph = "/Users/worawich/Google_Drive/WORK/opas_bgi_set2/ctk_result_for_post_process/sig_peak_non.bedgraph"
 
 stat_inf = open(stat_inf_filename,'w')
 stat_non = open(stat_non_filename,'w')
 sig_inf_peak = open(sig_inf_peak_bedgraph,'w')
 sig_non_peak = open(sig_non_peak_bedgraph,'w')
-
 
 read_pool_inf = np.loadtxt(fname=useable_read_pool_inf,dtype="str")
 total_read_pool_inf = read_pool_inf.shape[0]
@@ -48,32 +72,35 @@ total_read_sm_non = read_sm_non.shape[0]
 pool_inf_peak = np.loadtxt(fname=pool_inf_peak_bed,dtype="str")
 pool_non_peak = np.loadtxt(fname=pool_non_peak_bed,dtype="str")
 
-union_bedgraph = np.loadtxt(fname=union_bedgraph_ctk,dtype="str")
+#union_bedgraph = np.loadtxt(fname=union_bedgraph_ctk,dtype="str")
 
 union_count_dict = {}
-pool_inf_count_dict = utils.createCountDictFromBedgraph(pool_inf_bedgraph)
-pool_non_count_dict = utils.createCountDictFromBedgraph(pool_non_bedgraph)
-sm_inf_count_dict = utils.createCountDictFromBedgraph(sm_inf_bedgraph)
-sm_non_count_dict = utils.createCountDictFromBedgraph(sm_non_bedgraph)
+pool_inf_count_dict = ctk_utils.createCountDictFromBedgraph(pool_inf_bedgraph)
+pool_non_count_dict = ctk_utils.createCountDictFromBedgraph(pool_non_bedgraph)
+sm_inf_count_dict = ctk_utils.createCountDictFromBedgraph(sm_inf_bedgraph)
+sm_non_count_dict = ctk_utils.createCountDictFromBedgraph(sm_non_bedgraph)
 
 
-
-
+#Convert to RPM bedgraph
+ctk_utils.convert2rpmBedgraph(pool_inf_bedgraph,total_read_pool_inf)
+ctk_utils.convert2rpmBedgraph(pool_non_bedgraph,total_read_pool_non)
+ctk_utils.convert2rpmBedgraph(sm_inf_bedgraph,total_read_sm_inf)
+ctk_utils.convert2rpmBedgraph(sm_inf_bedgraph,total_read_sm_inf)
 
 # Loop create universal dict count. Value column represent inf non_inf sm_inf sm_non_inf
-first_flag = True
-for union_data in union_bedgraph:
+#first_flag = True
+#for union_data in union_bedgraph:
 
-    if(first_flag == True):
+    #if(first_flag == True):
 
-        union_count_dict['header'] = [union_data[3],union_data[4],union_data[5],union_data[6]]
-        first_flag = False
-    else:
-        start = int(union_data[1])
-        stop = int(union_data[2])
+     #   union_count_dict['header'] = [union_data[3],union_data[4],union_data[5],union_data[6]]
+      #  first_flag = False
+    #else:
+        #start = int(union_data[1])
+        #stop = int(union_data[2])
 
-        for i in range(start,stop+1):
-            union_count_dict[i]=[union_data[3],union_data[4],union_data[5],union_data[6]]
+        #for i in range(start,stop+1):
+            #union_count_dict[i]=[union_data[3],union_data[4],union_data[5],union_data[6]]
 
 # Prerequisit data
 pool_inf_pm_factor = total_read_pool_inf/1000000
@@ -94,16 +121,16 @@ for peak_data in pool_inf_peak:
     treat_inf_peak = int(peak_data[4])
 
     treat_inf_other = total_read_pool_inf - treat_inf_peak
-
-    for pos in range(start,stop+1):
+    sm_inf_count = 0
+    for pos in range(start,stop):
 
         try:
-            count = union_count_dict[pos][5]
+            count = sm_inf_count_dict[pos]
             sm_inf_count = sm_inf_count + count
         except:
             count=0
 
-    sm_inf_peak = sm_inf_count/((stop-start)+1)
+    sm_inf_peak = round(sm_inf_count/(stop-start))
     sm_inf_other = total_read_sm_inf - sm_inf_peak
 
     # Fisher exact test
@@ -124,7 +151,7 @@ for peak_data in pool_inf_peak:
     if(rpm_sm_inf_peak == 0):
         rpm_sm_inf_peak = 1
     ratio = rpm_treat_inf_peak/rpm_sm_inf_peak
-    l2fold= math.log(ratio,2)
+    l2fold = round(math.log(ratio,2))
 
     stat_inf.write(peak_id + "\t" + str(start) + "\t" + str(stop) + "\t"+ str(l2fold) + "\t" + str(oddsratio) + "\t" + str(p_value) + "\n")
 
@@ -137,7 +164,7 @@ sig_inf_peak.close()
 ########################################################
 
 # Loop non peak. Fisher exact test + log2 fold change
-stat_non.write("ID\tPOS_START\tPOS_STOP\tlog2FoldChange\tOdds_ratio\tP_Value\tDegree_freedom\n")
+stat_non.write("ID\tPOS_START\tPOS_STOP\tlog2FoldChange\tOdds_ratio\tP_Value\n")
 sig_non_peak.write("track type=bedGraph name=\"Significant peak (non-infect)\"\n")
 for peak_data in pool_non_peak:
     chromosome = peak_data[0]
@@ -147,16 +174,16 @@ for peak_data in pool_non_peak:
     treat_non_peak = int(peak_data[4])
 
     treat_non_other = total_read_pool_non - treat_non_peak
-
-    for pos in range(start,stop+1):
+    sm_non_count = 0
+    for pos in range(start,stop):
 
         try:
-            count = union_count_dict[pos][5]
+            count = sm_non_count_dict[pos]
             sm_non_count = sm_non_count + count
         except:
             count=0
 
-    sm_non_peak = sm_non_count/((stop-start)+1)
+    sm_non_peak = round(sm_non_count/(stop-start))
     sm_non_other = total_read_sm_non - sm_non_peak
 
     # Fisher exact test
@@ -177,7 +204,7 @@ for peak_data in pool_non_peak:
     if(rpm_sm_non_peak == 0):
         rpm_sm_non_peak = 1
     ratio = rpm_treat_non_peak/rpm_sm_non_peak
-    l2fold= math.log(ratio,2)
+    l2fold = round(math.log(ratio,2))
 
     stat_non.write(peak_id + "\t" + str(start) + "\t" + str(stop) + "\t"+ str(l2fold) + "\t" + str(oddsratio) + "\t" + str(p_value) + "\n")
 
