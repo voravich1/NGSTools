f865406368507a234edeb6c7238f6eaf3f0f7a4f
vorav <voravich1@hotmail.com>
2019-08-26 00:38:25 +0700

    -change open file method to withopen to avoid single line file error from nploadtext -add empty file check on peak file (terminate when peak file is empty) -other bug fixed

--- post_process_stat_ctk.py
+++ post_process_stat_ctk.py
@@ -1,3 +1,4 @@
+import os
 import ctk_utils
 import numpy as np
 import math
@@ -5,23 +6,31 @@ from scipy.stats import fisher_exact
 from pathlib import Path
 
 input_folder = "C:/Users/vorav/Desktop/opas_bgi_set2/test_post_ctk"
-p = Path("C:/Users/vorav/Desktop/opas_bgi_set2/test_post_ctk")
+p = Path("C:/Users/vorav/Desktop/opas_bgi_set2/ctk_post_hg38_n")
 
-useable_read_pool_inf = p / "pool_virus_inf_r2_pool.bed"
-useable_read_pool_non = p / "pool_virus_non_r2_pool.bed"
-useable_read_sm_inf = p / "sm_virus_inf_r2_pool.bed"
-useable_read_sm_non = p / "sm_virus_non_r2_pool.bed"
+useable_read_pool_inf = p / "pool_inf_hg38_n_pool.bed"
+useable_read_pool_non = p / "pool_non_hg38_n_pool.bed"
+useable_read_sm_inf = p / "sm_inf_hg38_n_pool.bed"
+useable_read_sm_non = p / "sm_non_hg38_n_pool.bed"
 
-pool_inf_peak_bed = p / "pool_virus_inf_r2_pool_peak_sig.bed"
-pool_non_peak_bed = p / "pool_virus_non_r2_pool_peak_sig.bed"
-sm_inf_peak_bed = p / "sm_virus_inf_r2_pool_peak_sig.bed"
-sm_non_peak_bed = p / "sm_virus_non_r2_pool_peak_sig.bed"
+pool_inf_peak_bed = p / "pool_inf_hg38_n_pool_peak_sig.bed"
+pool_non_peak_bed = p / "pool_non_hg38_n_pool_peak_sig.bed"
+#sm_inf_peak_bed = p / "sm_virus_inf_r2_pool_peak_sig.bed"
+#sm_non_peak_bed = p / "sm_virus_non_r2_pool_peak_sig.bed"
 
 #union_bedgraph_ctk = "/drives/c/Users/vorav/Desktop/opas_bgi_set2/test_post_ctk/union.bedgraph"
-pool_inf_bedgraph = p / "pool_virus_inf_r2_pool.bedgraph"
-pool_non_bedgraph = p / "pool_virus_non_r2_pool.bedgraph"
-sm_inf_bedgraph = p / "sm_virus_inf_r2_pool.bedgraph"
-sm_non_bedgraph = p / "sm_virus_non_r2_pool.bedgraph"
+pool_inf_bedgraph = p / "pool_inf_hg38_n_pool.bedgraph"
+pool_non_bedgraph = p / "pool_non_hg38_n_pool.bedgraph"
+sm_inf_bedgraph = p / "sm_inf_hg38_n_pool.bedgraph"
+sm_non_bedgraph = p / "sm_non_hg38_n_pool.bedgraph"
+
+# Check peak File is empty or not
+if(os.stat(pool_inf_peak_bed).st_size == 0): # Will return true if empty
+    print("inf peak file is empty. Can not process")
+    exit()
+elif(os.stat(pool_non_peak_bed).st_size == 0):
+    print("non peak file is empty. Can not process")
+    exit()
 
 # Save File name
 stat_inf_filename = p / "stat_inf.txt"
@@ -69,8 +78,8 @@ total_read_sm_inf = read_sm_inf.shape[0]
 read_sm_non = np.loadtxt(fname=useable_read_sm_non,dtype="str")
 total_read_sm_non = read_sm_non.shape[0]
 
-pool_inf_peak = np.loadtxt(fname=pool_inf_peak_bed,dtype="str")
-pool_non_peak = np.loadtxt(fname=pool_non_peak_bed,dtype="str")
+#pool_inf_peak = np.loadtxt(fname=pool_inf_peak_bed,dtype="str")
+#pool_non_peak = np.loadtxt(fname=pool_non_peak_bed,dtype="str")
 
 #union_bedgraph = np.loadtxt(fname=union_bedgraph_ctk,dtype="str")
 
@@ -113,50 +122,57 @@ sm_non_count = 0
 # Loop inf peak. Fisher exact test + log2 fold change
 stat_inf.write("ID\tPOS_START\tPOS_STOP\tlog2FoldChange\tOdds_ratio\tP_Value\n")
 sig_inf_peak.write("track type=bedGraph name=\"Significant peak (infect)\"\n")
-for peak_data in pool_inf_peak:
-    chromosome = peak_data[0]
-    start = int(peak_data[1])
-    stop = int(peak_data[2])
-    peak_id = peak_data[3]
-    treat_inf_peak = int(peak_data[4])
 
-    treat_inf_other = total_read_pool_inf - treat_inf_peak
-    sm_inf_count = 0
-    for pos in range(start,stop):
+with open(pool_inf_peak_bed,"r") as pool_inf_peak:
 
-        try:
-            count = sm_inf_count_dict[pos]
-            sm_inf_count = sm_inf_count + count
-        except:
-            count=0
+    for line in pool_inf_peak:
+        if(line == "\n"):
+            break
 
-    sm_inf_peak = round(sm_inf_count/(stop-start))
-    sm_inf_other = total_read_sm_inf - sm_inf_peak
+        peak_data = line.split("\t")
+        chromosome = peak_data[0]
+        start = int(peak_data[1])
+        stop = int(peak_data[2])
+        peak_id = peak_data[3]
+        treat_inf_peak = int(peak_data[4])
 
-    # Fisher exact test
-    input_array_table = np.array([[treat_inf_peak, treat_inf_other], [sm_inf_peak, sm_inf_other]])
-    res = fisher_exact(input_array_table)
-    oddsratio = res[0]
-    p_value = res[1]
-    #degree_freedom = res[2]
-    #exp_val = res[3]
-    # expect_value = "t_f:"+str(exp_val[0,0])+"|t_uf:"+str(exp_val[0,1])+"|ut_f:"+str(exp_val[1,0])+"|ut_uf:"+str(exp_val[1,1])
-    # expect_value = str(round(exp_val[0, 0],6)) + "\t" + str(round(exp_val[0, 1],6)) + "\t" + str(round(exp_val[1, 0],6)) + "\t" + str(round(exp_val[1, 1],6))
-    #expect_value = str(exp_val[0, 0]) + "\t" + str(exp_val[0, 1]) + "\t" + str(exp_val[1, 0]) + "\t" + str(exp_val[1, 1])
+        treat_inf_other = total_read_pool_inf - treat_inf_peak
+        sm_inf_count = 0
+        for pos in range(start,stop):
 
-    # log2 fold change
-    rpm_treat_inf_peak = treat_inf_peak/pool_inf_pm_factor
-    rpm_sm_inf_peak = sm_inf_peak/sm_inf_pm_factor
-    # check to prevent zero divide
-    if(rpm_sm_inf_peak == 0):
-        rpm_sm_inf_peak = 1
-    ratio = rpm_treat_inf_peak/rpm_sm_inf_peak
-    l2fold = round(math.log(ratio,2))
+            try:
+                count = sm_inf_count_dict[pos]
+                sm_inf_count = sm_inf_count + count
+            except:
+                count=0
 
-    stat_inf.write(peak_id + "\t" + str(start) + "\t" + str(stop) + "\t"+ str(l2fold) + "\t" + str(oddsratio) + "\t" + str(p_value) + "\n")
+        sm_inf_peak = round(sm_inf_count/(stop-start))
+        sm_inf_other = total_read_sm_inf - sm_inf_peak
 
-    if(p_value<0.05):
-        sig_inf_peak.write(chromosome + "\t" + str(start) + "\t" + str(stop) + "\t" + str(rpm_treat_inf_peak) + "\n")
+        # Fisher exact test
+        input_array_table = np.array([[treat_inf_peak, treat_inf_other], [sm_inf_peak, sm_inf_other]])
+        res = fisher_exact(input_array_table)
+        oddsratio = res[0]
+        p_value = res[1]
+        #degree_freedom = res[2]
+        #exp_val = res[3]
+        # expect_value = "t_f:"+str(exp_val[0,0])+"|t_uf:"+str(exp_val[0,1])+"|ut_f:"+str(exp_val[1,0])+"|ut_uf:"+str(exp_val[1,1])
+        # expect_value = str(round(exp_val[0, 0],6)) + "\t" + str(round(exp_val[0, 1],6)) + "\t" + str(round(exp_val[1, 0],6)) + "\t" + str(round(exp_val[1, 1],6))
+        #expect_value = str(exp_val[0, 0]) + "\t" + str(exp_val[0, 1]) + "\t" + str(exp_val[1, 0]) + "\t" + str(exp_val[1, 1])
+
+        # log2 fold change
+        rpm_treat_inf_peak = treat_inf_peak/pool_inf_pm_factor
+        rpm_sm_inf_peak = sm_inf_peak/sm_inf_pm_factor
+        # check to prevent zero divide
+        if(rpm_sm_inf_peak == 0):
+            rpm_sm_inf_peak = 1
+        ratio = rpm_treat_inf_peak/rpm_sm_inf_peak
+        l2fold = round(math.log(ratio,2))
+
+        stat_inf.write(peak_id + "\t" + str(start) + "\t" + str(stop) + "\t"+ str(l2fold) + "\t" + str(oddsratio) + "\t" + str(p_value) + "\n")
+
+        if(p_value<0.05):
+            sig_inf_peak.write(chromosome + "\t" + str(start) + "\t" + str(stop) + "\t" + str(rpm_treat_inf_peak) + "\n")
 
 stat_inf.close()
 sig_inf_peak.close()
@@ -166,50 +182,57 @@ sig_inf_peak.close()
 # Loop non peak. Fisher exact test + log2 fold change
 stat_non.write("ID\tPOS_START\tPOS_STOP\tlog2FoldChange\tOdds_ratio\tP_Value\n")
 sig_non_peak.write("track type=bedGraph name=\"Significant peak (non-infect)\"\n")
-for peak_data in pool_non_peak:
-    chromosome = peak_data[0]
-    start = int(peak_data[1])
-    stop = int(peak_data[2])
-    peak_id = peak_data[3]
-    treat_non_peak = int(peak_data[4])
 
-    treat_non_other = total_read_pool_non - treat_non_peak
-    sm_non_count = 0
-    for pos in range(start,stop):
+with open(pool_non_peak_bed,"r") as pool_non_peak:
+    for line in pool_non_peak:
+        peak_data = line.split("\t")
+        if (line == "\n"):
+            break
 
-        try:
-            count = sm_non_count_dict[pos]
-            sm_non_count = sm_non_count + count
-        except:
-            count=0
+        peak_data = line.split("\t")
+        chromosome = peak_data[0]
+        start = int(peak_data[1])
+        stop = int(peak_data[2])
+        peak_id = peak_data[3]
+        treat_non_peak = int(peak_data[4])
 
-    sm_non_peak = round(sm_non_count/(stop-start))
-    sm_non_other = total_read_sm_non - sm_non_peak
+        treat_non_other = total_read_pool_non - treat_non_peak
+        sm_non_count = 0
+        for pos in range(start,stop):
 
-    # Fisher exact test
-    input_array_table = np.array([[treat_non_peak, treat_non_other], [sm_non_peak, sm_non_other]])
-    res = fisher_exact(input_array_table)
-    oddsratio = res[0]
-    p_value = res[1]
-    #degree_freedom = res[2]
-    #exp_val = res[3]
-    # expect_value = "t_f:"+str(exp_val[0,0])+"|t_uf:"+str(exp_val[0,1])+"|ut_f:"+str(exp_val[1,0])+"|ut_uf:"+str(exp_val[1,1])
-    # expect_value = str(round(exp_val[0, 0],6)) + "\t" + str(round(exp_val[0, 1],6)) + "\t" + str(round(exp_val[1, 0],6)) + "\t" + str(round(exp_val[1, 1],6))
-    #expect_value = str(exp_val[0, 0]) + "\t" + str(exp_val[0, 1]) + "\t" + str(exp_val[1, 0]) + "\t" + str(exp_val[1, 1])
+            try:
+                count = sm_non_count_dict[pos]
+                sm_non_count = sm_non_count + count
+            except:
+                count=0
 
-    # log2 fold change
-    rpm_treat_non_peak = treat_non_peak/pool_non_pm_factor
-    rpm_sm_non_peak = sm_non_peak/sm_non_pm_factor
-    # check to prevent zero divide
-    if(rpm_sm_non_peak == 0):
-        rpm_sm_non_peak = 1
-    ratio = rpm_treat_non_peak/rpm_sm_non_peak
-    l2fold = round(math.log(ratio,2))
+        sm_non_peak = round(sm_non_count/(stop-start))
+        sm_non_other = total_read_sm_non - sm_non_peak
 
-    stat_non.write(peak_id + "\t" + str(start) + "\t" + str(stop) + "\t"+ str(l2fold) + "\t" + str(oddsratio) + "\t" + str(p_value) + "\n")
+        # Fisher exact test
+        input_array_table = np.array([[treat_non_peak, treat_non_other], [sm_non_peak, sm_non_other]])
+        res = fisher_exact(input_array_table)
+        oddsratio = res[0]
+        p_value = res[1]
+        #degree_freedom = res[2]
+        #exp_val = res[3]
+        # expect_value = "t_f:"+str(exp_val[0,0])+"|t_uf:"+str(exp_val[0,1])+"|ut_f:"+str(exp_val[1,0])+"|ut_uf:"+str(exp_val[1,1])
+        # expect_value = str(round(exp_val[0, 0],6)) + "\t" + str(round(exp_val[0, 1],6)) + "\t" + str(round(exp_val[1, 0],6)) + "\t" + str(round(exp_val[1, 1],6))
+        #expect_value = str(exp_val[0, 0]) + "\t" + str(exp_val[0, 1]) + "\t" + str(exp_val[1, 0]) + "\t" + str(exp_val[1, 1])
 
-    if(p_value<0.05):
-        sig_non_peak.write(chromosome + "\t" + str(start) + "\t" + str(stop) + "\t" + str(rpm_treat_non_peak) + "\n")
+        # log2 fold change
+        rpm_treat_non_peak = treat_non_peak/pool_non_pm_factor
+        rpm_sm_non_peak = sm_non_peak/sm_non_pm_factor
+        # check to prevent zero divide
+        if(rpm_sm_non_peak == 0):
+            rpm_sm_non_peak = 1
+        ratio = rpm_treat_non_peak/rpm_sm_non_peak
+        l2fold = round(math.log(ratio,2))
+
+        stat_non.write(peak_id + "\t" + str(start) + "\t" + str(stop) + "\t"+ str(l2fold) + "\t" + str(oddsratio) + "\t" + str(p_value) + "\n")
+
+        if(p_value<0.05):
+            sig_non_peak.write(chromosome + "\t" + str(start) + "\t" + str(stop) + "\t" + str(rpm_treat_non_peak) + "\n")
 
 stat_non.close()
 sig_non_peak.close()
